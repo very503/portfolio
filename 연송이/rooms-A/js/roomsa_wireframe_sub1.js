e3e790d31b564c2545368b54dec05f2f2dff48f0
ssong09140613 <dus203@hanmail.net>
2022-04-27 09:30:25 +0900

    1차

---
+++ 연송이/rooms-A/js/roomsa_wireframe_sub1.js
@@ -0,0 +1,20 @@
+$(function(){
+    var i = 0;
+    $('.header>.nav>li').last().click(function(){
+        $('.mbnav').css('display' , 'block');
+    });
+     $('.bar>li').first().click(function(){
+         $('.mbnav').css('display' , 'none');
+    })
+    $('.tab>li').click(function(){
+        $('.tab>li').removeClass('show');
+        $(this).addClass('show');
+        var ind = $(this).index();
+        $('.artGroup').css('margin-left',ind*-100+'%');
+        i=ind;
+    });
+
+    $('.ftrTopRight>.nav').click(function(){
+        $('html,body').stop().animate({ scrollTop : 0 },500);
+    });
+});
